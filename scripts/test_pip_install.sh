d68394c04c5dd6b0ef5f7dafdbc18cbddf65d6b6
CARLENA COLE <88572752+carlena3497@users.noreply.github.com>
2022-07-18 19:53:40 -0400

    Initialize docs_tweak

---
+++ scripts/test_pip_install.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -x
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+# shellcheck source=/dev/null
+. "$ROOT/scripts/common.sh"
+
+# Check that all required env var are set
+require_env_var GOOGLE_CLOUD_PROJECT "Set this to the project_id used for integration testing."
+require_env_var CLOUDDQ_BIGQUERY_DATASET "Set this to the BigQuery dataset used for integration testing."
+require_env_var CLOUDDQ_BIGQUERY_REGION "Set this to the BigQuery region used for integration testing."
+
+# set variables
+# if running locally you'd have to ensure the following are correctly set for your project/auth details
+GOOGLE_CLOUD_PROJECT="${GOOGLE_CLOUD_PROJECT}"
+CLOUDDQ_BIGQUERY_DATASET="${CLOUDDQ_BIGQUERY_DATASET}" 
+CLOUDDQ_BIGQUERY_REGION="${CLOUDDQ_BIGQUERY_REGION}"
+GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS:-}"
+IMPERSONATION_SERVICE_ACCOUNT="${IMPERSONATION_SERVICE_ACCOUNT:-}"
+
+# get diagnostic info
+which python3
+python3 --version
+
+# create temporary virtualenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+pyenv virtualenv clouddq || true
+pyenv shell clouddq
+pip3 --version
+
+# install clouddq wheel into temporary env
+pip3 install .
+
+# test clouddq help
+python3 clouddq --help
+
+# test clouddq in isolated directory with minimal file dependencies
+TEST_DIR=/tmp/clouddq-test-pip
+rm -rf "$TEST_DIR"
+mkdir "$TEST_DIR"
+cp -r tests/resources/configs "$TEST_DIR"
+cp tests/resources/test_dbt_profiles_dir/profiles.yml "$TEST_DIR"
+cd "$TEST_DIR"
+
+# update profiles.yml
+sed -i s/clouddq/"${CLOUDDQ_BIGQUERY_DATASET}"/g "$TEST_DIR"/profiles.yml
+sed -i s/\<your_gcp_project_id\>/"${GOOGLE_CLOUD_PROJECT}"/g "$TEST_DIR"/profiles.yml
+sed -i s/EU/"${CLOUDDQ_BIGQUERY_REGION}"/g "$TEST_DIR"/profiles.yml
+
+# update configs/entities/test-data.yml
+sed -i s/\<your_gcp_project_id\>/"${GOOGLE_CLOUD_PROJECT}"/g "$TEST_DIR"/configs/entities/test-data.yml
+sed -i s/\<your_bigquery_dataset_id\>/"${CLOUDDQ_BIGQUERY_DATASET}"/g "$TEST_DIR"/configs/entities/test-data.yml
+
+# update dataplex entity_uris
+# configs/rule_bindings/team-4-rule-bindings.yml
+sed -i s/\<my-gcp-dataplex-lake-id\>/"${DATAPLEX_LAKE_NAME}"/g "$TEST_DIR"/configs/rule_bindings/team-4-rule-bindings.yml
+sed -i s/\<my-gcp-dataplex-region-id\>/"${DATAPLEX_REGION_ID}"/g "$TEST_DIR"/configs/rule_bindings/team-4-rule-bindings.yml
+sed -i s/\<my-gcp-project-id\>/"${GOOGLE_CLOUD_PROJECT}"/g "$TEST_DIR"/configs/rule_bindings/team-4-rule-bindings.yml
+sed -i s/\<my-gcp-dataplex-zone-id\>/"${DATAPLEX_ZONE_ID}"/g "$TEST_DIR"/configs/rule_bindings/team-4-rule-bindings.yml
+sed -i s/\<my_bigquery_dataset_id\>/"${DATAPLEX_BIGQUERY_DATASET_ID}"/g "$TEST_DIR"/configs/rule_bindings/team-4-rule-bindings.yml
+# configs/metadata_registry_defaults.yml
+sed -i s/\<my-gcp-dataplex-lake-id\>/"${DATAPLEX_LAKE_NAME}"/g "$TEST_DIR"/configs/metadata_registry_defaults.yml
+sed -i s/\<my-gcp-dataplex-region-id\>/"${DATAPLEX_REGION_ID}"/g "$TEST_DIR"/configs/metadata_registry_defaults.yml
+sed -i s/\<my-gcp-project-id\>/"${GOOGLE_CLOUD_PROJECT}"/g "$TEST_DIR"/configs/metadata_registry_defaults.yml
+sed -i s/\<my-gcp-dataplex-zone-id\>/"${DATAPLEX_ZONE_ID}"/g "$TEST_DIR"/configs/metadata_registry_defaults.yml
+
+# run with --dbt_profiles_dir
+python3 -m clouddq ALL configs --dbt_profiles_dir="$TEST_DIR"  \
+    --debug  \
+    --dry_run \
+    --enable_experimental_bigquery_entity_uris
+python3 -m clouddq ALL configs --dbt_profiles_dir="$TEST_DIR" --dbt_path="$TEST_DIR"  \
+    --debug  \
+    --dry_run \
+    --enable_experimental_bigquery_entity_uris
+
+# test clouddq with direct connection profiles
+python3 -m clouddq ALL configs \
+    --gcp_project_id="${GOOGLE_CLOUD_PROJECT}" \
+    --gcp_bq_dataset_id="${CLOUDDQ_BIGQUERY_DATASET}" \
+    --gcp_region_id="${CLOUDDQ_BIGQUERY_REGION}" \
+    --debug \
+    --dry_run \
+    --enable_experimental_bigquery_entity_uris
+
+if [[ -f "${GOOGLE_APPLICATION_CREDENTIALS:-}" ]]; then
+    # test clouddq with exported service account key
+    python3 -m clouddq ALL configs \
+        --gcp_project_id="${GOOGLE_CLOUD_PROJECT}" \
+        --gcp_bq_dataset_id="${CLOUDDQ_BIGQUERY_DATASET}" \
+        --gcp_region_id="${CLOUDDQ_BIGQUERY_REGION}" \
+        --gcp_service_account_key_path="${GOOGLE_APPLICATION_CREDENTIALS}" \
+        --debug \
+        --dry_run \
+        --enable_experimental_bigquery_entity_uris
+    if [[ -f "${IMPERSONATION_SERVICE_ACCOUNT:-}" ]]; then
+        # test clouddq with exported service account key
+        python3 -m clouddq ALL configs \
+            --gcp_project_id="${GOOGLE_CLOUD_PROJECT}" \
+            --gcp_bq_dataset_id="${CLOUDDQ_BIGQUERY_DATASET}" \
+            --gcp_region_id="${CLOUDDQ_BIGQUERY_REGION}" \
+            --gcp_service_account_key_path="${GOOGLE_APPLICATION_CREDENTIALS}" \
+            --gcp_impersonation_credentials="${IMPERSONATION_SERVICE_ACCOUNT}" \
+            --debug \
+            --dry_run \
+            --enable_experimental_bigquery_entity_uris
+    fi
+fi
+
+if [[ -f "${IMPERSONATION_SERVICE_ACCOUNT:-}" ]]; then
+    # test clouddq with service account impersonation
+    python3 -m clouddq ALL configs \
+        --gcp_project_id="${GOOGLE_CLOUD_PROJECT}" \
+        --gcp_bq_dataset_id="${CLOUDDQ_BIGQUERY_DATASET}" \
+        --gcp_region_id="${CLOUDDQ_BIGQUERY_REGION}" \
+        --gcp_impersonation_credentials="${IMPERSONATION_SERVICE_ACCOUNT}" \
+        --debug \
+        --dry_run \
+        --enable_experimental_bigquery_entity_uris
+fi
---
+++ tests/integration/test_bigquery_client.py
@@ -0,0 +1,98 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.api_core.exceptions import BadRequest
+
+import pytest
+
+from clouddq.integration.bigquery.bigquery_client import BigQueryClient
+
+
+@pytest.mark.e2e
+class TestBigQueryClient:
+
+    @pytest.fixture(scope="session")
+    def client(self):
+        """Get BigQuery Client using discovered ADC"""
+        client = BigQueryClient()
+        yield client
+        client.close_connection()
+
+    def test_check_query_dry_run_failure(self, client):
+        query = "SELECT 1; drop table students;--"
+        with pytest.raises(BadRequest):
+            client.check_query_dry_run(query)
+
+    def test_check_query_dry_run(self, client):
+        query = "SELECT 1 + 2"
+        client.check_query_dry_run(query)
+
+    def test_assert_is_dataset_exists(self, client):
+        table_name = "bigquery-public-data.github_repos.commits"
+        table_ref = client.table_from_string(
+            table_name
+        )
+        project_id = table_ref.project
+        dataset_id = table_ref.dataset_id
+        assert client.is_dataset_exists(dataset=dataset_id, project_id=project_id) is True
+
+    def test_assert_is_dataset_exists_implicit_project_id(self, client, gcp_dataplex_bigquery_dataset_id):
+        assert client.is_dataset_exists(dataset=gcp_dataplex_bigquery_dataset_id) is True
+
+    def test_assert_dataset_is_in_region(self, client):
+        dataset = "bigquery-public-data.google_analytics_sample"
+        region = "US"
+        client.assert_dataset_is_in_region(dataset=dataset, region=region)
+
+    def test_assert_is_table_exists(self, client):
+        table_name = "bigquery-public-data.github_repos.commits"
+        table_ref = client.table_from_string(
+            table_name
+        )
+        project_id = table_ref.project
+        assert client.is_table_exists(table=table_name, project_id=project_id) is True
+
+    def test_assert_is_table_exists_implicit_project_id(self,
+            client,
+            gcp_project_id,
+            target_bq_result_dataset_name,
+            target_bq_result_table_name):
+        table = f"{gcp_project_id}.{target_bq_result_dataset_name}.{target_bq_result_table_name}"
+        assert client.is_table_exists(table=table) is True
+
+    def test_assert_required_columns_exist_in_table(self,
+            client,
+            gcp_project_id,
+            target_bq_result_dataset_name,
+            target_bq_result_table_name):
+        table = f"{gcp_project_id}.{target_bq_result_dataset_name}.{target_bq_result_table_name}"
+        client.assert_required_columns_exist_in_table(table=table, project_id=gcp_project_id)
+
+    def test_assert_required_columns_exist_in_table_implicit_project_id(self,
+            client,
+            gcp_project_id,
+            target_bq_result_dataset_name,
+            target_bq_result_table_name):
+        table = f"{gcp_project_id}.{target_bq_result_dataset_name}.{target_bq_result_table_name}"
+        client.assert_required_columns_exist_in_table(table=table)
+
+    def test_assert_dataset_is_in_region_failure(self, client):
+        dataset = "bigquery-public-data.google_analytics_sample"
+        region = "EU"
+        with pytest.raises(AssertionError):
+            client.assert_dataset_is_in_region(dataset=dataset, region=region)
+
+
+if __name__ == "__main__":
+    raise SystemExit(pytest.main([__file__, '-vv', '-rP', '-n', 'auto']))
